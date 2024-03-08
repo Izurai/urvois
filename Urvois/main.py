ddf88f153296d506d5b1d947e0caaa2f851c6185
Izurai <85119385+Izurai@users.noreply.github.com>
2024-03-08 13:46:52 +0100

    Create main.py
    Just what an hour of math should look like in our math professor's class.

---
+++ Urvois/main.py
@@ -0,0 +1,14 @@
+from language import french
+from math import cours
+for i in mathExperte :
+  print("Bonjour")
+while time!=17h55 :
+  while mainLevee==false :
+    cours("Complexes")
+  if mainLevee==true :
+    repondre(question)
+if devoirCorrige==true :
+  for i in mathExperte :
+    give(devoir[i],i)
+  else
+    cours.end
